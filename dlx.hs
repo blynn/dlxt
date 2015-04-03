6948a83e8e05c467a06ee59565292d72ac804116
Ben Lynn <benlynn@gmail.com>
2015-04-02 23:34:07 -0700

    Compiles with no warnings.

--- dlx.hs
+++ dlx.hs
@@ -2,7 +2,6 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
 import Data.Array.ST
-import Data.STRef
 import Data.List
 import Data.List.Split
 import Data.Char
@@ -11,6 +10,7 @@ import Data.Char
 --               , [1, 2, 3]
 --               , [1, 2]
 --               , [0]]
+dlxSolve :: [[Int]] -> [Int] -> [[Int]]
 dlxSolve rows picks = runST $ do
   let
     cols = 1 + maximum (concat rows)
@@ -19,36 +19,31 @@ dlxSolve rows picks = runST $ do
   -- 0 = up, 1 = down, 2 = left, 3 = right, 4 = num
   tmp <- newArray (0, 5*(1 + cols + ct) - 1) $ 0 :: ST s (STUArray s Int Int)
   let
-    connectUD i j = writeArray tmp (i+1) j >> writeArray tmp j i
-    connectLR i j = writeArray tmp (i+3) j >> writeArray tmp (j+2) i
+    linkUD i j = writeArray tmp (i+1) j >> writeArray tmp j i
+    linkLR i j = writeArray tmp (i+3) j >> writeArray tmp (j+2) i
 
     addRow r cs = let 
       n = length cs
       f k c = let cn = 5*(c+1) in do
         up <- readArray tmp cn
-        connectUD up (g k)
-        connectUD (g k) cn
+        linkUD up (g k)
+        linkUD (g k) cn
         writeArray tmp (g k + 4) cn
-        readArray tmp (cn + 4) >>= writeArray tmp (cn + 4) . (1+)
+        readArray tmp (cn + 4) >>= writeArray tmp (cn + 4) . (+1)
         return (k + 1)
-
-      g k = 5*(k+r)
+      g i = 5*(i+r)
       in do
-        foldM f 1 cs
-        zipWithM connectLR (g<$>(n:[1..])) (g<$>[1..n])
+        foldM_ f 1 cs
+        zipWithM_ linkLR (g<$>(n:[1..])) (g<$>[1..n])
         return $ r + n
-{-
-    todo = let
-      f 0 = return []
-      f c = do
-        s <- readArray tmp (c + 4)
-        ((c, s):) <$> (f <=< readArray tmp $ c + 3)
-      in readArray tmp 3 >>= f
--}
 
-    todo = do
-      cs <- go 3 0
-      zip cs <$> mapM (readArray tmp . (+ 4)) cs
+  forM_ ((5*)<$>(cols:[1..cols]))
+    $ \c -> writeArray tmp c c >> writeArray tmp (c+1) c
+  zipWithM_ linkLR ((5*)<$>(cols:[0..])) ((5*)<$>[0..cols])
+  foldM_ addRow cols rows
+
+  let
+    todo = go 3 0 >>= \cs -> zip cs <$> mapM (readArray tmp . (+ 4)) cs
 
     go dir c0 = let
       f c
@@ -56,32 +51,30 @@ dlxSolve rows picks = runST $ do
         | True    = (c:) <$> (readArray tmp (c + dir) >>= f)
       in readArray tmp (c0 + dir) >>= f
 
-    coverCol c = do
-      left <- readArray tmp (c + 2)
-      right <- readArray tmp (c + 3)
-      connectLR left right
+    coverCol col = do
       -- Slower:
-      -- join $ connectLR <$> readArray tmp (c + 2) <*> readArray tmp (c + 3)
-      go 1 c >>= mapM_ (go 3 >=> mapM_ (\c -> do
+      --   linkLR left right
+      -- Even slower:
+      --   join $ linkLR <$> readArray tmp (col + 2) <*> readArray tmp (col + 3)
+      left <- readArray tmp (col + 2)
+      right <- readArray tmp (col + 3)
+      writeArray tmp (left+3) right >> writeArray tmp (right+2) left
+      go 1 col >>= mapM_ (go 3 >=> mapM_ (\c -> do
         cn <- readArray tmp (c + 4)
         readArray tmp (cn + 4) >>= writeArray tmp (cn + 4) . (+ (-1))
         up <- readArray tmp c
         down <- readArray tmp $ c + 1
-        connectUD up down))
+        -- Slower: linkUD up down
+        writeArray tmp (up+1) down >> writeArray tmp down up))
 
-    uncoverCol c = do
-      go 0 c >>= mapM_ (go 2 >=> mapM_ (\c -> do
+    uncoverCol col = do
+      go 0 col >>= mapM_ (go 2 >=> mapM_ (\c -> do
         cn <- readArray tmp (c + 4)
         readArray tmp (cn + 4) >>= writeArray tmp (cn + 4) . (+ 1)
         readArray tmp c >>= flip (writeArray tmp) c . (+ 1)
         readArray tmp (c + 1) >>= flip (writeArray tmp) c))
-      readArray tmp (c + 2) >>= flip (writeArray tmp) c . (+ 3)
-      readArray tmp (c + 3) >>= flip (writeArray tmp) c . (+ 2)
-
-  forM_ ((5*)<$>(cols:[1..cols]))
-    $ \c -> writeArray tmp c c >> writeArray tmp (c+1) c
-  zipWithM connectLR ((5*)<$>(cols:[0..])) ((5*)<$>[0..cols])
-  foldM addRow cols rows
+      readArray tmp (col + 2) >>= flip (writeArray tmp) col . (+ 3)
+      readArray tmp (col + 3) >>= flip (writeArray tmp) col . (+ 2)
 
   let
     findRow n = snd . head
@@ -92,58 +85,58 @@ dlxSolve rows picks = runST $ do
       case cs of
         [] -> return $ [findRow <$> sol]
         _  -> let
-          (c, s) = foldl1' (\a b -> if snd b < snd a then b else a) cs
+          (col, s) = foldl1' (\a b -> if snd b < snd a then b else a) cs
           in if s == 0 then return [] else do
-            coverCol c
-            sols <- go 1 c >>= mapM (\c -> do
-              go 3 c >>= mapM_ (\c -> readArray tmp (c + 4) >>= coverCol)
-              sols <- solve (c:sol)
-              go 2 c >>= mapM_ (\c -> readArray tmp (c + 4) >>= uncoverCol)
+            coverCol col
+            sols <- go 1 col >>= mapM (\col' -> do
+              go 3 col' >>= mapM_ (\c -> readArray tmp (c + 4) >>= coverCol)
+              sols <- solve (col':sol)
+              go 2 col' >>= mapM_ (\c -> readArray tmp (c + 4) >>= uncoverCol)
               return sols)
-            uncoverCol c
+            uncoverCol col
             return $ concat sols
 
   forM_ picks (\r -> let c = (0:csum)!!r in
-    -- Ignore empty rows.
-    if c == csum!!r then return () else let n = 5*(c + cols + 1) in do
-      readArray tmp (n + 4) >>= coverCol
-      go 3 n >>= mapM_ (\c -> readArray tmp (c + 4) >>= coverCol))
+    if c == csum!!r then return ()  -- Ignore empty rows.
+      else let n = 5*(c + cols + 1) in do
+        readArray tmp (n + 4) >>= coverCol
+        go 3 n >>= mapM_ ((coverCol =<<) . readArray tmp . (+ 4)))
        
   solve []
 
--- Example from http://en.wikipedia.org/wiki/Knuth's_Algorithm_X
-wpex = ((+(-1)) <$>) <$>
-  [ [1, 4, 7]
-  , [1, 4]
-  , [4, 5, 7]
-  , [3, 5, 6]
-  , [2, 3, 6, 7]
-  , [2, 7]
-  ]
-
-wpmain = putStr . unlines $ show <$> dlxSolve wpex []
+{-
 main0 = print $ dlxSolve [ [0, 3] , [1, 2, 3] , [1, 2] , [0]] []
 
-sudex = concat
-  [ ".......1."
-  , "4........"
-  , ".2......."
-  , "....5.4.7"
-  , "..8...3.."
-  , "..1.9...."
-  , "3..4..2.."
-  , ".5.1....."
-  , "...8.6..."
-  ]
+main1 = let
+  -- Example from http://en.wikipedia.org/wiki/Knuth's_Algorithm_X
+  ex = ((+(-1)) <$>) <$>
+    [ [1, 4, 7]
+    , [1, 4]
+    , [4, 5, 7]
+    , [3, 5, 6]
+    , [2, 3, 6, 7]
+    , [2, 7]
+    ]
+  in  putStr . unlines $ show <$> dlxSolve ex []
+-}
 
-b9 a b c = 81*a + 9*b + c
-
-con r c d = [b9 0 r c, b9 1 r d, b9 2 c d, b9 3 ((div r 3)*3+(div c 3)) d]
-
-g ch n = 9*n + digitToInt ch - 1
-
-board = uncurry g <$> filter ((/='.') . fst) (zip sudex [0..])
-
-ds = [0..8]
-
-main = putStr . unlines . ((intToDigit <$>)<$>) . chunksOf 9 . ((+1) . (`mod`9) <$>) . sort $ board ++ (head $ dlxSolve (con <$> ds <*> ds <*> ds) board)
+main :: IO ()
+main = let
+  ex = concat
+    [ ".......1."
+    , "4........"
+    , ".2......."
+    , "....5.4.7"
+    , "..8...3.."
+    , "..1.9...."
+    , "3..4..2.."
+    , ".5.1....."
+    , "...8.6..."
+    ]
+  b9 a b c = 81*a + 9*b + c
+  con r c d = [b9 0 r c, b9 1 r d, b9 2 c d, b9 3 ((div r 3)*3+(div c 3)) d]
+  board = (\(ch, n) -> 9*n + digitToInt ch - 1)
+    <$> filter ((/='.') . fst) (zip ex [0..])
+  ds = [0..8]
+  in putStr . unlines . ((intToDigit <$>)<$>) . chunksOf 9 . ((+1) . (`mod`9)
+    <$>) . sort $ board ++ (head $ dlxSolve (con <$> ds <*> ds <*> ds) board)
